5a01621a0f4eb08b69957236a1ae2d7de25120a7
Déborah Dobles <126917588+deborahdobles@users.noreply.github.com>
2023-04-22 03:00:48 +0000

    Final html, css, js

--- src/app.js
+++ src/app.js
@@ -5,7 +5,36 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  let numbers = [
+    "A",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let suits = ["♦", "♥", "♠", "♣"];
+
+  let randomIndex = Math.floor(Math.random() * suits.length);
+  let randomSuit = suits[randomIndex];
+
+  document.getElementById("top-suit").innerHTML = randomSuit;
+  document.getElementById("bottom-suit").innerHTML = randomSuit;
+
+  if (randomSuit === "♦" || randomSuit === "♥") {
+    document.getElementById("top-suit").classList.add("red");
+    document.getElementById("bottom-suit").classList.add("red");
+  }
+
+  let randomNumberIndex = Math.floor(Math.random() * numbers.length);
+  document.getElementById("card-number").innerHTML = numbers[randomNumberIndex];
 };
