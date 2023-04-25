48fa7145a19e988a597e83a593fc579c5bff7bc2
Déborah Dobles <126917588+deborahdobles@users.noreply.github.com>
2023-04-25 02:41:14 +0000

    updated js and html

--- src/app.js
+++ src/app.js
@@ -5,30 +5,45 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = () => {
-  let numbers = [
-    "A",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "J",
-    "Q",
-    "K"
-  ];
-  let suits = ["♦", "♥", "♠", "♣"];
+let numbers = [
+  "A",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+let suits = ["♦", "♥", "♠", "♣"];
 
-  let randomIndex = Math.floor(Math.random() * suits.length);
-  let randomSuit = suits[randomIndex];
+let randomIndex = Math.floor(Math.random() * suits.length);
+let randomSuit = suits[randomIndex];
 
+document.getElementById("top-suit").innerHTML = randomSuit;
+document.getElementById("bottom-suit").innerHTML = randomSuit;
+
+if (randomSuit === "♦" || randomSuit === "♥") {
+  document.getElementById("top-suit").classList.add("red");
+  document.getElementById("bottom-suit").classList.add("red");
+}
+
+let randomNumberIndex = Math.floor(Math.random() * numbers.length);
+document.getElementById("card-number").innerHTML = numbers[randomNumberIndex];
+
+function generateRandomCard() {
+  randomIndex = Math.floor(Math.random() * suits.length);
+  randomSuit = suits[randomIndex];
   document.getElementById("top-suit").innerHTML = randomSuit;
   document.getElementById("bottom-suit").innerHTML = randomSuit;
+  document.getElementById("top-suit").classList.remove("red");
+  document.getElementById("bottom-suit").classList.remove("red");
 
   if (randomSuit === "♦" || randomSuit === "♥") {
     document.getElementById("top-suit").classList.add("red");
@@ -37,4 +52,30 @@ window.onload = () => {
 
   let randomNumberIndex = Math.floor(Math.random() * numbers.length);
   document.getElementById("card-number").innerHTML = numbers[randomNumberIndex];
-};
+}
+
+generateRandomCard();
+
+setInterval(generateRandomCard, 10000);
+
+const cardElement = document.getElementById("card");
+const cardWidthInput = document.getElementById("card-width");
+const cardHeightInput = document.getElementById("card-height");
+
+cardWidthInput.addEventListener("input", changeCardSize);
+cardHeightInput.addEventListener("input", changeCardSize);
+
+function changeCardSize() {
+  const cardElement = document.getElementById("card");
+  console.log(cardHeightInput);
+  if (cardHeightInput.value >= 0) {
+    cardElement.style.height = cardHeightInput.value + "px";
+  } else {
+    cardElement.style.height = "400px";
+  }
+  if (cardWidthInput.value >= 0) {
+    cardElement.style.width = cardWidthInput.value + "px";
+  } else {
+    cardElement.style.width = "300px";
+  }
+}
